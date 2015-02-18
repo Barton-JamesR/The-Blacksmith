f0f6b93a9e962ee982c383f5098de452e01c150d
Kit Vander Wilt <mathdawg51@gmail.com>
2015-02-17 18:10:31 -0700

    Milestone 1 files
    I added all the files I had for the milestone 1 project.

---
+++ The Blacksmith/Milestone 1/Assets/EndGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+
+	public GameObject thingy;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		thingy.guiText.text = string.Format ("Timer: {0:F1}", Time.time);
+
+		if (Time.time >= 60f) {
+			guiText.text = "Game Over";
+		}
+	}
+
+
+}
---
+++ The Blacksmith/Milestone 1/Submit/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	public GameObject player;
+
+	private Vector3 offsetPosition;
+	private Quaternion offsetRotation;
+
+	// Use this for initialization
+	void Start () {
+		offsetPosition = player.transform.position;
+		offsetPosition.y = offsetPosition.y + 0.7f;
+		offsetRotation = player.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.rotation = player.transform.rotation;
+		transform.position = player.transform.position + offsetPosition; 
+	}
+}
