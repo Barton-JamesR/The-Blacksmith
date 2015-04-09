6b9bc74c9a70969216f5c6c6258addba7ceaf2dd
barton.jamesr <barton.jamesr@gmail.com>
2015-04-09 16:02:12 -0600

    Please disregard this, Git

---
+++ The Blacksmith/Milestone 1/Assets/Menu/EndingGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndingGame : MonoBehaviour {
+
+	public void OnMouseDown () {
+		if (this.name == "door") {
+			// load the stats panel
+			Debug.Log("door was clicked");
+			Application.LoadLevel("EndStatistics");
+		}
+		if (this.tag == "MenuButton" ) {
+			// load the main menu
+			Application.LoadLevel ("MainMenu");
+		}
+		if (this.tag == "ExitButton") {
+			//  Exit the game
+			Application.Quit();
+		}
+	}
+}
